33bc1444cdf9a9f6782fefeecc55aedc23d03148
mon0macal7 <monhdz07@gmail.com>
2021-10-05 10:17:50 -0500

    cambios en js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,15 @@
 window.cipher = {
-  // ...
+  encode:(offset,string) => { 
+    for (let i=0; i<(string.length); i++){
+      let asciiPosition= string.charCodeAt (i);
+      console.log('asciiPosition');
+      (x -65 + n) % 26 +65*/
+                let enCodexString = (asciiPosition-65 + offset) %26 + 65;
+                let result = String.fromCharCode(enCodexString);
+              }
+              return result;
+          }
+      };
+
 };
+encode();
---
+++ src/index.js
@@ -0,0 +1,10 @@
+alert("Bienvenido");
+
+
+const offset = document.getElementById('offset-box').value;
+//console.log(key);
+let string = document.getElementById('decodex-string').value;
+//console.log(codexString);
+//Imprimir resultado en el input correspondiente
+let string = document.getElementById('decodex-string').value;
+//console.log(codexString);
