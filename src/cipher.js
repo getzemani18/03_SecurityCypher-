d8d5f5b0d5030ec75bee1550fd8ac487f4181ab3
mon0macal7 <monhdz07@gmail.com>
2021-10-05 13:49:53 -0500

    cambios para test

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,14 @@
 let cifrado;
 window.cipher = {
-<<<<<<< HEAD
-  encode:(offset,string) => { 
-    for (let i=0; i<(string.length); i++){
-      let asciiPosition= string.charCodeAt (i);
-      console.log('asciiPosition');
-      (x -65 + n) % 26 +65 
-                let enCodexString = (asciiPosition-65 + offset) %26 + 65;
-                let result = String.fromCharCode(enCodexString);
-              }
-              return result;
-          }
-      };
-encode();
-=======
-    encode: function
-  
+  encode: function (offset,str){
+    console.log('aqui va la funcion encoding')
+
+    cifrado = offset + str 
+    return(typeof(cifrado))
+    
+  },
+  decode: function(){
+    console.log("aqui va el codigo de la funcion de la decode")
+  }
 };
->>>>>>> 9e4222041937601f1cb48d73077ba854f0a8f24f
+console.log(cipher.encode(3,"hola"))
--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
-<<<<<<< HEAD
 alert("Bienvenido");
 
-
-const offset = document.getElementById('offset-box').value;
-//console.log(key);
-let string = document.getElementById('decodex-string').value;
-//console.log(codexString);
-//Imprimir resultado en el input correspondiente
-let string = document.getElementById('decodex-string').value;
-//console.log(codexString);
-=======
-alert ('Bienvenido a SegurityCypher')
-
-
-
->>>>>>> 9e4222041937601f1cb48d73077ba854f0a8f24f
