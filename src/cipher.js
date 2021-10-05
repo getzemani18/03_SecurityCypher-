00e47b434db754e397e7b7403bbfca75f025685d
mon0macal7 <monhdz07@gmail.com>
2021-10-05 12:36:40 -0500

    agregue funcion encode en js

--- src/cipher.js
+++ src/cipher.js
@@ -3,13 +3,11 @@ window.cipher = {
     for (let i=0; i<(string.length); i++){
       let asciiPosition= string.charCodeAt (i);
       console.log('asciiPosition');
-      (x -65 + n) % 26 +65*/
+      (x -65 + n) % 26 +65 
                 let enCodexString = (asciiPosition-65 + offset) %26 + 65;
                 let result = String.fromCharCode(enCodexString);
               }
               return result;
           }
       };
-
-};
 encode();
