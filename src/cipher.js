629018a97a3457e8ab95ac5f8b2911b2c10101f3
getzemani18 <amandaalvarado0129@gmail.com>
2021-10-05 23:14:29 -0500

    botonces css

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,11 @@
-let cifrado;
+
 window.cipher = {
-<<<<<<< HEAD
-  encode:(offset,string) => { 
-    for (let i=0; i<(string.length); i++){
-      let asciiPosition= string.charCodeAt (i);
-      console.log('asciiPosition');
-      (x -65 + n) % 26 +65 
-                let enCodexString = (asciiPosition-65 + offset) %26 + 65;
-                let result = String.fromCharCode(enCodexString);
-              }
-              return result;
-          }
-      };
-encode();
-=======
-    encode: function
-  
-};
->>>>>>> 9e4222041937601f1cb48d73077ba854f0a8f24f
+  encode:(offset,string) => {
+ console.log("cifrado")
+let cifrado= "";
+for(let i=0, i<cifrar.length, i++) {
+
+}
+  }
+      
+
