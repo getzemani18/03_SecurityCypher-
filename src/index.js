28e01849359779f2c2887be6d566f1196b5711c7
getzemani18 <amandaalvarado0129@gmail.com>
2021-10-06 12:09:39 -0500

    codigo en JvaScriptpara codificar y decodificar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-alert("Bienvenido");
+alert("Bienvenido al Cifrado Cesar"); //alert de bienvenida
 
 
 
-let contraseña = prompt('Ingrsa tu contraseña o pseudocodigo')
+// prompt para la contraseña de ingreso a la web
+let contraseña = prompt('Ingresa tu contraseña o pseudocodigo') 
 if (contraseña == 3) {
 alert('contraseña correcta')
 } else {
 alert('Incorrecto')
 }
 document.getElementById("respuestacorrecta").innerHTML = contraseña;
+
+
+
+
+
+// Abecedario para crear el string 
