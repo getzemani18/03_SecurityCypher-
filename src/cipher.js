1db9b9b2f3d0d85dad92e058a23100659d12be6b
getzemani18 <amandaalvarado0129@gmail.com>
2021-10-06 11:27:19 -0500

    modifica el js y html

--- src/cipher.js
+++ src/cipher.js
@@ -12,3 +12,18 @@ window.cipher = {
           }
       };
 encode();
+
+let decifrado;
+window.cipher = {
+  decode:(offset,string) => {
+    for (let i=0; i< string.length ; i++){
+      let asciiPosition= string.charCodeAt (i);
+      console.log('asciiPosition');
+      (x -65 + n) % 26 +65 
+                let enCodexString = (asciiPosition-65 + offset) %26 + 65;
+                let result = String.fromCharCode(enCodexString);
+              }
+              return result;
+          }
+  }
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 alert("Bienvenido");
 
+
+
+let contraseña = prompt('Ingrsa tu contraseña o pseudocodigo')
+if (contraseña == 3) {
+alert('contraseña correcta')
+} else {
+alert('Incorrecto')
+}
+document.getElementById("respuestacorrecta").innerHTML = contraseña;
