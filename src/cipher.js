05dbfd821e5fd3658677c7c93dff1d16de6b750c
getzemani18 <amandaalvarado0129@gmail.com>
2021-10-05 23:15:46 -0500

    probando

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,14 @@
-
+let cifrado;
 window.cipher = {
-  encode:(offset,string) => {
- console.log("cifrado")
-let cifrado= "";
-for(let i=0, i<cifrar.length, i++) {
-
-}
-  }
-      
-
+  encode:(offset,string) => { 
+    for (let i=0; i< string.length ; i++){
+      let asciiPosition= string.charCodeAt (i);
+      console.log('asciiPosition');
+      (x -65 + n) % 26 +65 
+                let enCodexString = (asciiPosition-65 + offset) %26 + 65;
+                let result = String.fromCharCode(enCodexString);
+              }
+              return result;
+          }
+      };
+encode();
